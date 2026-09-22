8030701ddfa57bb4c210a53df31e8fcf098fd026
ALINAITWE DOROTHY <alinaitwedorothy15@gmail.com>
2026-09-22 11:45:33 +0300

    Initial commit

---
+++ costfrontend/script.js
@@ -0,0 +1,7 @@
+const addProjectButton = document.getElementById("addProjectBtn");
+
+addProjectButton.addEventListener("click", function () {
+
+    alert("Project Management module will be developed next.");
+
+});
